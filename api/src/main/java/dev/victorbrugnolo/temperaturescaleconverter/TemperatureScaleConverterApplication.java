851a57cfe79d8df4c6b1dca72759d9e4f6e6f870
victorbrugnolo <brugnolovictor@gmail.com>
2019-09-09 22:00:21 -0300

    iniciando o projeto

---
+++ api/src/main/java/dev/victorbrugnolo/temperaturescaleconverter/TemperatureScaleConverterApplication.java
@@ -0,0 +1,13 @@
+package dev.victorbrugnolo.temperaturescaleconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureScaleConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureScaleConverterApplication.class, args);
+	}
+
+}
