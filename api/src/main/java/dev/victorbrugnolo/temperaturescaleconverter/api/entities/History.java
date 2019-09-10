c2b8e01f3bcfce73eaad0fddbf683b156d88267b
victorbrugnolo <brugnolovictor@gmail.com>
2019-09-09 23:29:44 -0300

    entidade e ajuste no tipo de campo do db

---
+++ api/src/main/java/dev/victorbrugnolo/temperaturescaleconverter/api/entities/History.java
@@ -0,0 +1,78 @@
+package dev.victorbrugnolo.temperaturescaleconverter.api.entities;
+
+import dev.victorbrugnolo.temperaturescaleconverter.api.enums.TemperatureEnum;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "history")
+public class History {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "original_value")
+    private double originalValue;
+
+    @NotNull
+    @Column(name = "converted_value")
+    private double convertedValue;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "original_scale_temp")
+    private TemperatureEnum originalScaleTemp;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "convertedTo")
+    private TemperatureEnum convertedTo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public double getOriginalValue() {
+        return originalValue;
+    }
+
+    public void setOriginalValue(double originalValue) {
+        this.originalValue = originalValue;
+    }
+
+    public double getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(double convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public TemperatureEnum getOriginalScaleTemp() {
+        return originalScaleTemp;
+    }
+
+    public void setOriginalScaleTemp(TemperatureEnum originalScaleTemp) {
+        this.originalScaleTemp = originalScaleTemp;
+    }
+
+    public TemperatureEnum getConvertedTo() {
+        return convertedTo;
+    }
+
+    public void setConvertedTo(TemperatureEnum convertedTo) {
+        this.convertedTo = convertedTo;
+    }
+
+    @Override
+    public String toString() {
+        return "History{" +
+                "id=" + id +
+                ", originalValue=" + originalValue +
+                ", convertedValue=" + convertedValue +
+                ", originalScaleTemp='" + originalScaleTemp + '\'' +
+                ", convertedTo='" + convertedTo + '\'' +
+                '}';
+    }
+}
