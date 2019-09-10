5f7204246181bb15c63293ef55593e1075380b59
victorbrugnolo <brugnolovictor@gmail.com>
2019-09-10 20:39:32 -0300

    testando service de conversão de temperaturas

---
+++ api/src/test/java/dev/victorbrugnolo/temperaturescaleconverter/api/services/ConversionTempScaleServiceTests.java
@@ -0,0 +1,36 @@
+package dev.victorbrugnolo.temperaturescaleconverter.api.services;
+
+import dev.victorbrugnolo.temperaturescaleconverter.api.dtos.ConvertedTemperatureResponse;
+import dev.victorbrugnolo.temperaturescaleconverter.api.enums.TemperatureEnum;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class ConversionTempScaleServiceTests {
+
+    @Autowired
+    ConversionTempScaleService conversionTempScaleService;
+
+    @Test
+    public void shouldBeConvertFromCelsiusToFarenheit() {
+        ConvertedTemperatureResponse converted = conversionTempScaleService.convert(35, TemperatureEnum.FARENHEIT);
+        assertEquals(95.00000, converted.getConvertedValue(), 0.1);
+    }
+
+    @Test
+    public void shouldBeConvertFromFarenheitToCelsius() {
+        ConvertedTemperatureResponse converted = conversionTempScaleService.convert(15, TemperatureEnum.CELSIUS);
+        assertEquals(-9.444444, converted.getConvertedValue(), 0.1);
+    }
+
+}
