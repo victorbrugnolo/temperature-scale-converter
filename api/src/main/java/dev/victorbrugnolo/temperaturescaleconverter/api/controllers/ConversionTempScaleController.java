de86f7f9d1aede1d6459301c10059d2bd932240f
victorbrugnolo <brugnolovictor@gmail.com>
2019-09-10 21:57:13 -0300

    adicionado swagger

--- api/src/main/java/dev/victorbrugnolo/temperaturescaleconverter/api/controllers/ConversionTempScaleController.java
+++ api/src/main/java/dev/victorbrugnolo/temperaturescaleconverter/api/controllers/ConversionTempScaleController.java
@@ -3,11 +3,14 @@ package dev.victorbrugnolo.temperaturescaleconverter.api.controllers;
 import dev.victorbrugnolo.temperaturescaleconverter.api.dtos.ConvertedTemperatureResponse;
 import dev.victorbrugnolo.temperaturescaleconverter.api.enums.TemperatureEnum;
 import dev.victorbrugnolo.temperaturescaleconverter.api.services.ConversionTempScaleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Api(value = "Converter", description = "REST API for convert temperature scales", tags = { "Convert" })
 @RequestMapping("/api/convert")
 @CrossOrigin(origins = "*")
 public class ConversionTempScaleController {
@@ -16,6 +19,7 @@ public class ConversionTempScaleController {
     ConversionTempScaleService conversionService;
 
     @GetMapping
+    @ApiOperation(value = "Convert from Farenheit to Celsius and vice versa")
     public ResponseEntity<ConvertedTemperatureResponse> convert(@RequestParam("temperature") double temperature,
                                                                 @RequestParam("convertTo") TemperatureEnum convertTo) {
         return ResponseEntity.ok(conversionService.convert(temperature, convertTo));
--- api/src/main/java/dev/victorbrugnolo/temperaturescaleconverter/api/controllers/HistoryController.java
+++ api/src/main/java/dev/victorbrugnolo/temperaturescaleconverter/api/controllers/HistoryController.java
@@ -4,6 +4,8 @@ import dev.victorbrugnolo.temperaturescaleconverter.api.entities.History;
 import dev.victorbrugnolo.temperaturescaleconverter.api.enums.TemperatureEnum;
 import dev.victorbrugnolo.temperaturescaleconverter.api.services.ConversionTempScaleService;
 import dev.victorbrugnolo.temperaturescaleconverter.api.services.HistoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Api(value = "History", description = "REST API for get history of conversions", tags = { "History" })
 @RequestMapping("/api/history")
 @CrossOrigin(origins = "*")
 public class HistoryController {
@@ -19,6 +22,7 @@ public class HistoryController {
     HistoryService historyService;
 
     @GetMapping
+    @ApiOperation(value = "Get list from all conversions realized")
     public ResponseEntity<List<History>> findAll() {
         return ResponseEntity.ok(historyService.findAll());
     }
